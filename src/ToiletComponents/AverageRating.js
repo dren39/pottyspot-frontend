880c9b79cdb2267d3ba876ccf92bf2ecd8d34931
dren39 <dren391@gmail.com>
2019-05-04 16:41:57 -0400

    added comments

--- src/ToiletComponents/AverageRating.js
+++ src/ToiletComponents/AverageRating.js
@@ -3,6 +3,11 @@ import {connect} from 'react-redux';
 
 class AverageRating extends React.Component {
 
+  state = {
+    average: 0,
+    averageLength: 0
+  };
+
   componentDidMount() {
     this.calculateAverageRating()
   }
@@ -20,40 +25,53 @@ class AverageRating extends React.Component {
   }
 
   changeStarColor = (adjustedAverage) => {
+    //this function changes the color of the stars
       // let element = document.getElementById(adjustedAverage);
       let strId = "";
       let i = 6;
       while (i <= adjustedAverage) {
+        //while i is less than the average passed in arguement run the loop
         strId = i.toString()
         let gold = document.getElementById(strId)
+        //grab the element who's integer id is equal to the value of i at each iteration
         gold.style.color = "gold";
+        //change the color of the found element to gold
         i++;
       };
       while (i <= 10) {
         strId = i.toString()
         let white = document.getElementById(strId)
+        //grab the element who's integer id is equal to the value of i at each iteration
         white.style.color = "black";
+        //change the color of the found element to black
         i++;
       };
   };
 
   sum = (total,num) => {
+    //this is the callback function used by the .reduce() to compare two elements in the array
     return total+num;
   };
 
  calculateAverageRating = () => {
-   console.log("I'm being called from average rating");
     const ratings = [];
     this.props.toilet.ratings.forEach(rating => {
+      //iterate over the array of rating objects in the toilet object (provided by serializer) and push the value at a key of value into the ratings array
       ratings.push(rating.value)
     })
     const average = Math.round((ratings.reduce(this.sum,0)/ratings.length)*10)/10
+    //average is the average of all the ratings for the specific toilet object, rounded to the 10th place
     const roundAverage = Math.floor(average)
+    //roundAverage rounds the average down to the nearest integer
     const adjustedAverage = roundAverage + 5
+    //adjustedAverage is roundAverage plus 5, this is because elements can't have the same id(1-5) so the stars in this component were given (5-10)
     this.changeStarColor(adjustedAverage)
+    //call changeStarColor function and pass in the adjustedAverage to grab the corresponding star element
+    // this.setState({average: average, averageLength: ratings.length}, ()=>this.changeStarColor(adjustedAverage))
   };
 
   componentDidUpdate = (prevProp) => {
+    //this checks to see if the toilet from the store being passed is still the same object as the toilet in the current store, if not then run calculateAverageRating again to dynamically change the star color.
     if (this.props.toilet !== prevProp) {
       this.calculateAverageRating()
     }
--- src/ToiletComponents/Rating.js
+++ src/ToiletComponents/Rating.js
@@ -6,30 +6,38 @@ class Rating extends Component {
 
   state = {
     rating: null,
-    ratingTracker: []
+    // ratingTracker: []
   };
 
   changeStarColor = (event) => {
+    //this changes the stars' style color to gold and black
     // let element = document.getElementById(event.target.id);
     let starId = parseInt(event.target.id);
+    //starId is equal to the integer id of the clicked star
     let strId = "";
     let i = 1;
     while (i <= starId) {
       strId = i.toString()
+      //strId is the stringify of whatever i is on each iteration
       let gold = document.getElementById(strId)
+      //on each iterating grab a star element who's integer id matches i
       gold.style.color = "gold";
       i++;
     };
     while (i <= 5) {
+      //i has been incremented up to the value of the clicked star
       strId = i.toString()
-      let white = document.getElementById(strId)
-      white.style.color = "black";
+      let black = document.getElementById(strId)
+      //grab the star element who's integer id matches i's current value
+      black.style.color = "black";
+      //turn the style color of each element black
       i++;
     };
   };
 
   createRating = () => {
-    const ratingTracker = []
+    //make a POST request to the Rating controller
+    // const ratingTracker = []
     fetch('http://localhost:4000/api/v1/ratings', {
       method: 'POST',
       headers: {
@@ -48,14 +56,17 @@ class Rating extends Component {
   };
 
   saveRating = () => {
+    //this function saves all the star elements who's color is gold to an array
     const goldStars = [];
     const stars = document.getElementsByClassName("star")
+    //return an array of elements who's className is "star" (always 5)
     for (let i = 0; i < stars.length; i++) {
       if (stars[i].style.color === "gold") {
+        //iterate over the stars array based on it's length and for each element who's style color is "gold" push into the goldStars array
         goldStars.push(stars[i].style.color)
       }
     };
-    console.log(goldStars);
+    //save the length of the goldStars array to local state, this presents the value rated by the user, and call the createRating function
     this.setState({rating: goldStars.length}, ()=>this.createRating())
   };
 
