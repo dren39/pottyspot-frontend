0481bd8b414d8186f02c5e6f05021ffc9f3e9edf
David Ren <dren391@gmail.com>
2020-07-02 17:14:04 -0400

    clean up un-used code

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import {connect} from 'react-redux';
 import {Route, Switch} from 'react-router-dom';
 import Home from './Home';
 import MyMapContainer from './MapComponents/MyMapContainer';
-import Signup from './UserComponents/Signup'
-import Login from './UserComponents/Login'
+import Signup from './UserComponents/Signup';
+import Login from './UserComponents/Login';
 
 class App extends Component {
 
   componentDidMount() {
-    //this grabs the user's current lat, lng from the browser the moment the application is started and sends it to the store
+    //this grabs the user's current lat, lng from the browser the moment the application 
+    // is started and sends it to the store
     navigator.geolocation.getCurrentPosition((position) => {
       this.props.dispatch({type:'set_lat_lng', lat:position.coords.latitude, lng:position.coords.longitude})
     })
